7979a5457d89a4d4f3ef65635998e42aaf993cdf
sins <sinsmailza@gmail.com>
2026-02-28 15:01:07 +0200

    chore: add toaster

--- src/components/providers/app-providers.tsx
+++ src/components/providers/app-providers.tsx
@@ -1,6 +1,12 @@
 import type { ReactNode } from "react";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/sonner";
 
 export const AppProviders = ({ children }: { children: ReactNode }) => {
-  return <TooltipProvider>{children}</TooltipProvider>;
+  return (
+    <TooltipProvider>
+      {children}
+      <Toaster position="bottom-center" />
+    </TooltipProvider>
+  );
 };
