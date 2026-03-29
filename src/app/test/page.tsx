48deab25d65056e74ef9ba0d8020f3c161da590f
sins <sinsmailza@gmail.com>
2026-03-29 23:13:29 +0200

    chore: add search query test

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,11 +1,25 @@
+import { CommandExample } from "@/components/input/command-example";
 import { Button } from "@/components/ui/button";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 
-export default function Page() {
-  return <div></div>
-  
+import { loadSearchParams } from "@/lib/nuqs/search";
+import type { SearchParams } from "nuqs/server";
+
+type Props = {
+  searchParams: Promise<SearchParams>;
+};
+
+export default async function Page({ searchParams }: Props) {
+  const { userId } = await loadSearchParams(searchParams);
+
+  const refreshSomePage = async () => {
+    "use server";
+    revalidatePath("/test", "page");
+  };
+
   const signIn = async () => {
     "use server";
     const response = await auth.api.signInSocial({
@@ -47,6 +61,18 @@ export default function Page() {
     if (response.status) redirect("/dashboard");
   };
 
+  const someMath = async () => {
+    "use server";
+    const num1 = 1;
+    const num2 = 2;
+    const result = num1 + num2;
+    console.log(result);
+  };
+
+  const postPromise = fetch(
+    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`,
+  ).then((res) => res.json());
+
   return (
     <>
       <form action={signIn}>
@@ -61,6 +87,11 @@ export default function Page() {
       <form action={unLinkJira}>
         <Button type="submit">Un-Link Jira Account</Button>
       </form>
+      <form action={someMath}>
+        <Button type="submit">Do Some Math</Button>
+      </form>
+      <Button onClick={refreshSomePage}>Click me</Button>
+      <CommandExample postPromise={postPromise} />
     </>
   );
 }
