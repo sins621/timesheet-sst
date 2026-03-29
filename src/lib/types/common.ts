ae704242222ff3e9b7ad6904e5b2e0ad7abf035c
sins <sinsmailza@gmail.com>
2026-03-30 00:44:35 +0200

    feat: api request types

--- src/lib/types/common.ts
+++ src/lib/types/common.ts
@@ -1,11 +1,22 @@
+import { Result } from "neverthrow";
+import { DomainError } from "../constants/errors/domainErrors";
+import { InfraError } from "../constants/errors/infraError";
+import { RouteError } from "../constants/errors/routeErrors";
 import { urlSchema } from "../schemas/common";
 import { z } from "zod";
 
 export type Url = z.infer<typeof urlSchema>;
-
+export type Error = InfraError | DomainError | RouteError;
+export type Method = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
 export type Endpoint = {
-  method: "GET" | "POST";
+  method: Method;
   url: Url;
 };
-
-export type AuthHeaders = { Authorization: string }
+export type AuthHeaders = { Authorization: string };
+export type ApiRequestOptions = {
+  params?: Record<string, string | number>;
+};
+export type ApiRequest<TResponse, TParams> = (
+  authHeaders: AuthHeaders,
+  searchParams?: TParams,
+) => Promise<Result<TResponse, Error>>;
