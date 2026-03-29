e97da4e1eb1b3b21e882a9ecba4577fcf91e9b2a
sins <sinsmailza@gmail.com>
2026-03-29 23:14:16 +0200

    chore: add providers comopnent

--- src/components/providers/app-providers.tsx
+++ src/components/providers/app-providers.tsx
@@ -1,12 +1,15 @@
 import type { ReactNode } from "react";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/sonner";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 export const AppProviders = ({ children }: { children: ReactNode }) => {
   return (
-    <TooltipProvider>
-      {children}
-      <Toaster position="bottom-center" />
-    </TooltipProvider>
+    <NuqsAdapter>
+      <TooltipProvider>
+        {children}
+        <Toaster position="bottom-center" />
+      </TooltipProvider>
+    </NuqsAdapter>
   );
 };
