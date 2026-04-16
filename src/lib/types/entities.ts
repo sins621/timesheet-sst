fa4d64e827c026f2b37416db05165abf4f0ba706
sins <sinsmailza@gmail.com>
2026-04-16 22:43:57 +0200

    chore: add stub type enum

--- src/lib/types/entities.ts
+++ src/lib/types/entities.ts
@@ -12,3 +12,16 @@ export type WarpAuth = {
 export type JiraAuth = {
   token: string;
 };
+
+export enum TicketType {
+  stationary = 0,
+  transitioned = 1,
+}
+
+export type MessageStub = {
+  id: number;
+  type: TicketType;
+  statusAId: number;
+  statusBId: number;
+  messageTemplateId: number;
+};
