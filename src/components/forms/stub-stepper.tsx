d5edce8944fe38a9c0a1b62540f59441386aa797
sins <sinsmailza@gmail.com>
2026-04-17 23:04:24 +0200

    chore: remove rhf

--- src/components/forms/stub-stepper.tsx
+++ src/components/forms/stub-stepper.tsx
@@ -1,30 +1,19 @@
 "use client";
 
-import { stubInputSchema } from "@/lib/schemas/forms";
-import {
-  StubMessageInputSchema,
-  StubStatusInputSchema,
-  StubTypeInputSchema
-} from "@/lib/types/forms";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { defineStepper } from "@stepperize/react";
-import { FormProvider, useForm, useFormContext } from "react-hook-form";
 
 const { Scoped, useStepper } = defineStepper(
   {
     id: "type",
     title: "Ticket Type",
-    schema: stubInputSchema.pick({ type: true }),
   },
   {
     id: "status",
     title: "Indicate Status",
-    schema: stubInputSchema.pick({ statusA: true, statusB: true }),
   },
   {
     id: "message",
     title: "Message",
-    schema: stubInputSchema.pick({ messageTemplate: true }),
   },
 );
 
@@ -39,90 +28,34 @@ export function StubStepperRoot() {
 
 export function StubStepperForm() {
   const stepper = useStepper();
-  const schema = stepper.state.current.data.schema;
-
-  const methods = useForm({
-    resolver: zodResolver(schema),
-    defaultValues: {
-      type: "",
-      statusA: "",
-      statusB: "",
-      messageTemplate: "",
-    },
-  });
 
   const onValid = () => {
     if (!stepper.state.isLast) stepper.navigation.next();
   };
 
   return (
-    <FormProvider {...methods}>
-      <form
-        onSubmit={methods.handleSubmit((value) => {
-          onValid();
-          console.log(value);
-        })}
-      >
-        {stepper.flow.switch({
-          type: () => <StubStepperType />,
-          status: () => <StubStepperStatus />,
-          message: () => <StubStepperMessage />,
-        })}
-        <button type="submit">Submit</button>
-      </form>
-    </FormProvider>
-  );
-}
-
-export function StubStepperNav() {
-  const stepper = useStepper();
-  return <div></div>;
-}
-
-export function StubStepperType() {
-  const form = useFormContext<StubTypeInputSchema>();
-  return (
-    <div>
-      <label>Name</label>
-      <input {...form.register("type")} />
-      {form.formState.errors.type && (
-        <span>{form.formState.errors.type.message}</span>
-      )}
-    </div>
-  );
-}
-
-export function StubStepperStatus() {
-  const form = useFormContext<StubStatusInputSchema>();
-  return (
     <>
-      <div>
-        <label>Name</label>
-        <input {...form.register("statusA")} />
-        {form.formState.errors.statusA && (
-          <span>{form.formState.errors.statusA.message}</span>
-        )}
-      </div>
-      <div>
-        <label>Name</label>
-        <input {...form.register("statusB")} />
-        {form.formState.errors.statusB && (
-          <span>{form.formState.errors.statusB.message}</span>
-        )}
-      </div>
+      {stepper.flow.switch({
+        type: () => <StubStepperType />,
+        status: () => <StubStepperStatus />,
+        message: () => <StubStepperMessage />,
+      })}
     </>
   );
 }
 
+export function StubStepperNav() {
+  return <div></div>;
+}
+
+export function StubStepperType() {
+  return <div></div>;
+}
+
+export function StubStepperStatus() {
+  return <div></div>;
+}
+
 export function StubStepperMessage() {
-  const form = useFormContext<StubMessageInputSchema>();
-  return (
-    <div>
-      <label>Name</label>
-      <input {...form.register("messageTemplate")} />
-      {form.formState.errors.messageTemplate && (
-        <span>{form.formState.errors.messageTemplate.message}</span>
-      )}
-    </div>
-  );
+  return <div></div>;
 }
