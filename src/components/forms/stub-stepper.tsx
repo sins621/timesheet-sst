ff10ca25ca9f60aa6e2e6197e9cbf808259bf627
sins <sinsmailza@gmail.com>
2026-04-18 16:18:26 +0200

    wip: stub stepper

--- src/components/forms/stub-stepper.tsx
+++ src/components/forms/stub-stepper.tsx
@@ -1,6 +1,11 @@
 "use client";
 
+import { OptionItem } from "@/lib/types/common";
 import { defineStepper } from "@stepperize/react";
+import TypeButton from "../buttons/type-button";
+import StatusSelect from "../select/status-select";
+import { Button } from "../ui/button";
+import { Progress } from "../ui/progress";
 
 const { Scoped, useStepper } = defineStepper(
   {
@@ -20,8 +25,11 @@ const { Scoped, useStepper } = defineStepper(
 export function StubStepperRoot() {
   return (
     <Scoped>
-      <StubStepperForm />
-      <StubStepperNav />
+      <div className="border p-2 flex flex-col gap-2">
+        <StubStepperProgress />
+        <StubStepperForm />
+        <StubStepperNav />
+      </div>
     </Scoped>
   );
 }
@@ -49,13 +57,116 @@ export function StubStepperNav() {
 }
 
 export function StubStepperType() {
-  return <div></div>;
+  const stepper = useStepper();
+
+  return (
+    <div className="flex flex-col gap-2">
+      <TypeButton
+        title="Stationary"
+        description="A ticket that has stayed in the same status for the day"
+        onClick={() => stepper.navigation.next()}
+      />
+      <TypeButton
+        title="Transitioned"
+        description="A ticket that has moved from one status to another"
+        onClick={() => stepper.navigation.next()}
+      />
+    </div>
+  );
 }
 
 export function StubStepperStatus() {
-  return <div></div>;
+  const stepper = useStepper();
+
+  const selectOneOptions: OptionItem[] = [
+    {
+      value: "1",
+      label: "Option 1",
+    },
+    {
+      value: "2",
+      label: "Option 2",
+    },
+  ];
+  const selectTwoOptions = [
+    {
+      value: "3",
+      label: "Status A",
+    },
+    {
+      value: "4",
+      label: "Option 2",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-2">
+      <StatusSelect
+        title="Starting Status"
+        placeholder="Option 1"
+        options={selectOneOptions}
+      />
+      <StatusSelect
+        title="Ending Status"
+        placeholder="Option 2"
+        options={selectTwoOptions}
+      />
+      <Button className="w-full" onClick={() => stepper.navigation.next()}>
+        Confirm
+      </Button>
+    </div>
+  );
 }
 
 export function StubStepperMessage() {
-  return <div></div>;
+  const messageOptions: OptionItem[] = [
+    {
+      value: "1",
+      label: "Option 1",
+    },
+    {
+      value: "2",
+      label: "Option 2",
+    },
+    {
+      value: "3",
+      label: "Status A",
+    },
+    {
+      value: "4",
+      label: "Option 2",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-2 justify-items-center">
+      {messageOptions.map((option) => (
+        <button key={option.value} className="border p-2">
+          <span>{option.label}</span>
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export function StubStepperProgress() {
+  const stepper = useStepper();
+
+  const title = stepper.state.current.data.title;
+  const stepOneProgress = stepper.state.isFirst ? 0 : 100;
+  const stepTwoProgress = stepper.state.isLast ? 100 : 0;
+
+  return (
+    <div className="grid grid-cols-3 gap-2 p-1 w-full">
+      <Progress
+        onClick={() => stepper.navigation.goTo("type")}
+        value={stepOneProgress}
+      />
+      <Progress
+        onClick={() => stepper.navigation.goTo("status")}
+        value={stepTwoProgress}
+      />
+      <Progress onClick={() => stepper.navigation.goTo("message")} value={0} />
+    </div>
+  );
 }
