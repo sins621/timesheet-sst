9f58b31e5020510edae3fe7078df3cd1df07511d
sins <sinsmailza@gmail.com>
2026-04-19 16:56:04 +0200

    chore: setup storybook vitest

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,7 @@ const config: StorybookConfig = {
   "stories": [
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
-  "addons": [],
+  "addons": ["@storybook/addon-vitest", "storybook-addon-test-codegen"],
   "framework": "@storybook/nextjs-vite",
   "staticDirs": [
     "../public"
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,12 @@
 import { configDefaults, defineConfig } from "vitest/config";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { playwright } from "@vitest/browser-playwright";
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
@@ -11,5 +17,37 @@ export default defineConfig({
     reporters: ["junit", "default"],
     outputFile: "./test-reports/vitest-junit.xml",
     exclude: [...configDefaults.exclude, "**/tests/**"],
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: "unit",
+          exclude: [
+            ...configDefaults.exclude,
+            "**/tests/**",
+            "src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+          ],
+        },
+      },
+      {
+        extends: true,
+        plugins: [
+          storybookTest({
+            configDir: path.join(dirname, ".storybook"),
+            storybookScript: "pnpm run storybook",
+          }),
+        ],
+        test: {
+          name: "storybook",
+          browser: {
+            enabled: true,
+            provider: playwright({}),
+            headless: true,
+            instances: [{ browser: "chromium" }],
+          },
+          setupFiles: ["./.storybook/vitest.setup.ts"],
+        },
+      },
+    ],
   },
 });
