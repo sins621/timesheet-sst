b5a4113633a8326baab48e385125fffb234e23e4
sins <sinsmailza@gmail.com>
2026-05-25 15:28:11 +0200

    chore: update formatting

--- src/lib/utils/common.ts
+++ src/lib/utils/common.ts
@@ -1,6 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-
 import type { AuthHeaders } from "../types/common";
 
 export function cn(...inputs: ClassValue[]) {
